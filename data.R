2121af4778313321ce2c43f5766290fd13bc158e
AkselA <akselahenriksen@gmail.com>
2017-10-02 18:58:52 +0200

    changed xyz and xyz2 slightly. renamed audio to stereo

--- data.R
+++ data.R
@@ -1,22 +1,23 @@
 
-### audio
+### stereo
 library(tuneR)
-audio <- readMP3("~/Documents/R/data/DR-test/ATribeCalledQuest_TheKillingSeason.mp3")
+stereo <- readMP3(paste0("~/Documents/R/data/DR-test/",
+                          "ATribeCalledQuest_TheKillingSeason.mp3"))
 
-l <- audio@left
-r <- audio@right
-audio <- data.frame(l, r)
+l <- stereo@left
+r <- stereo@right
+stereo <- data.frame(l, r)
 lnz <- which(l != 0)
 rnz <- which(r != 0)
 
-audio <- audio[min(c(lnz, rnz)):max(c(lnz, rnz)),]
-audio <- audio[200:22250, ]
-audio <- fade(audio, fin=c(0.5, 500, 500), fout=c(0, 3000, 3000))
-matplot(audio, type="l", lty=1)
+stereo <- stereo[min(c(lnz, rnz)):max(c(lnz, rnz)),]
+stereo <- stereo[200:22250, ]
+stereo <- fade(stereo, fin=c(0.5, 500, 500), fout=c(0, 3000, 3000))
+matplot(stereo, type="l", lty=1)
 
 ### xyz
 set.seed(1)
-l <- 270
+l <- 300
 per <- rep(c(1:9, 2:-6, -5:0, 9:1, -5:5, -3:8, 5:-4), length.out=l)/5
 per <- per + rnorm(l)/5
 x <- rnorm(l) + ema(per, 11)
@@ -26,9 +27,9 @@ xyz <- x
 
 ### xyz2
 xyz2 <- log(abs(xyz - 1))
-xyz2 <- rolliter(xyz2, 9, 9, TRUE) + xyz2/5
-xyz2 <- fade(xyz2, fin=c(0.25, 40, 20), fout=c(0.2, 20, 10))
-# plot(xyz2, col="#FF000088", lwd=1.5)
+xyz2 <- rolliter(xyz2, 9, 9, TRUE) - xyz2/5
+xyz2 <- fade(xyz2, fin=c(0.5, 40, 20), fout=c(0.4, 20, 10))
+# plot(xyz2, col="#FF0000FF", lwd=1.5, type="l", pch=16)
 
 ### remove intermediate files
 rm(l, r, lnz, rnz, per, x)
